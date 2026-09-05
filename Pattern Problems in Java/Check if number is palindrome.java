e33977d4e37de25184f150996afb9ccb6a957aab
Kishan V <kishanv922@gmail.com>
2026-09-05 13:33:28 +0530

    Add palindrome check for integers

---
+++ Pattern Problems in Java/Check if number is palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        while(x>0) {
+            int lastDigit= x%10;
+            rev= rev*10+ lastDigit;
+            x= x/10;
+        }
+        return rev;
+        
+    }
+    public boolean isPalindrome(int x) {
+        int m= reverse(x);
+        if(m==x) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+}
